829c80f484437e802487cedce04286e1af5d3f34
Shaun Lee <shaundlee.0103@gmail.com>
2020-08-19 19:46:26 -0400

    Added testing for fail

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -15,7 +15,16 @@ function success(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    item.durability -= 5
+    return {...item}
+  } else if(item.enhancement >= 15){
+    if(item.enhancement > 16){
+       item.enhancement -= 1
+    }
+    item.durability -= 10
+    return { ...item };
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -13,35 +13,44 @@ const kutum = {
 const nouver = {
     name: 'Nouver Dagger',
     durability: 100,
-    enhancement: 16
+    enhancement: 15
 }
 describe('enhancements', () => {
     describe('repairs', () => {
-        it.skip('repairs an item', () => {
-            expect(repair(kzarka)).toHaveProperty('durability',100)
-            expect(repair(kutum)).toHaveProperty('durability',100)
+        it('repairs an item', () => {
+            expect(repair({...kzarka})).toHaveProperty('durability',100)
+            expect(repair({...kutum})).toHaveProperty('durability',100)
         })
     })
     describe('failures', () => {
-        it.todo('reduces durability by 5 if below 15')
-        it.todo('reduces durability by 10 if 15 or more')
-        it.todo('if more than 16, it reduces the enhancement level')
-
+        it('reduces durability by 5 if below 15', () => {
+            expect(fail({...kzarka})).toHaveProperty('durability', 85)
+            expect(fail({...nouver})).not.toHaveProperty('durability', 95)
+        })
+        it('reduces durability by 10 if 15 or more', () => {
+            expect(fail({...nouver})).toHaveProperty('durability', 90)
+            expect(fail({...kutum})).toHaveProperty('durability', 65)
+            expect(fail({...kzarka})).not.toHaveProperty('durability', 80)
+        })
+        it('if more than 16, it reduces the enhancement level', () => {
+            expect(fail({...nouver})).toHaveProperty('enhancement', 15)
+            expect(fail({...kutum})).toHaveProperty('enhancement', 19)
+            expect(fail({...kzarka})).toHaveProperty('enhancement', 5)
+        })
     })
-    //     describe('successes', () => {
-    //     it('increases the enhancement level by 1', () => {
-    //         expect(success(kzarka)).toHaveProperty('enhancement', 6)
-    //         expect(success(nouver)).toHaveProperty('enhancement', 17)
-    //     })
-    //     it('does not go past 20', () => {
-    //         expect(success(kutum)).toHaveProperty('enhancement', 20)
-    //     })
-    //     it('does not change durability', () => {
-    //         expect(success(kutum)).toHaveProperty('durability', 75)
-    //         expect(success(kzarka)).toHaveProperty('durability', 90)
-    //         expect(success(nouver)).toHaveProperty('durability', 100)
-
-    //     })
-    // })
+    describe('successes', () => {
+        it('increases the enhancement level by 1', () => {
+            expect(success({...kzarka})).toHaveProperty('enhancement', 6)
+            expect(success({...nouver})).toHaveProperty('enhancement', 16)
+        })
+        it('does not go past 20', () => {
+            expect(success({...kutum})).toHaveProperty('enhancement', 20)
+        })
+        it('does not change durability', () => {
+            expect(success({...kutum})).toHaveProperty('durability', 75)
+            expect(success({...kzarka})).toHaveProperty('durability', 90)
+            expect(success({...nouver})).toHaveProperty('durability', 100)
+    })
+})
 
 })
